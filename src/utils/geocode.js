9ee352efad04c407c37988f13eee9e97c8e24455
Vitaliy Lytvyn <vitaliy.lytvyn@swanteams.com>
2019-11-14 00:10:34 +0200

    fix on object destructering in function parameter

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2t5ZnJlZTgyIiwiYSI6ImNrMnViejZvaDEwbjgzY3RqbnQ3ZG9uaHMifQ.-ciFRFIlAGwrzszXxpovsA&limit=1'
 
-    request({ url, json: true }, (error, { body }) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
         } else if (body.features.length === 0) {
